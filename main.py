4ee776c0947efdcf0ed78c938f830c6a5d536886
BLACKTARHEROIN-tar <sean_macariola@dlsu.edu.ph>
2025-10-20 04:08:27 +0800

    added arg parser using sys.argv

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from stats import get_book_text
 from stats import sort_dict
+import sys
 
-count_dict, count = get_book_text("books/frankenstein.txt")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+print(sys.argv)
+count_dict, count = get_book_text(sys.argv[1])
 sorted_list = sort_dict(count_dict)
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {sys.argv[1]}...")
 print("----------- Word Count ----------")
 print(f"Found {count} total words")
 print("--------- Character Count -------")
@@ -12,5 +17,3 @@ for dic in sorted_list:
     if dic["char"].isalpha():
         print(f"{dic["char"]}: {dic["num"]}")
 print("============= END ===============")
-    
-
